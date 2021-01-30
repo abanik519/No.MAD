a2ffc49e952971dd859ec37776667f59a67e9326
Victorien Garrigues <victoriengarrigues@VictoriensAir2.com>
2021-01-30 14:57:04 -0500

    pushing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import './styles/header.css';
 import './styles/searchbar.css';
+import './styles/featured.css';
 
 
 import Header from './components/header';
+import Featured from './components/featured';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Featured />
       </div>
     );
   }
