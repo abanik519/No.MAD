d7eff97f007176d63929f73845f2be3c47d21679
Victorien Garrigues <victoriengarrigues@VictoriensAir2.com>
2021-01-30 17:21:07 -0500

    finished menu

--- src/components/featured.js
+++ src/components/featured.js
@@ -4,29 +4,129 @@ export class Featured extends Component {
     state = {
         isSneaker: true,
         isStreetwear: false,
+        isWatch: false,
+        isHandbag: false,
+        isJewellery: false,
     };
 
     SneakerActive = () => {
         this.setState({ isSneaker: true});
         this.setState({ isStreetwear: false});
+        this.setState({ isWatch: false});
+        this.setState({ isHandbag: false});
+        this.setState({ isJewellery: false});
         if(
-          document.getElementById("sneakers")
+          document.getElementById("sneakers") &&
+          document.getElementById("onSneaker")
           ){
-            document.getElementById("sneakers").style.borderBottom = "10px solid #000000";
-            document.getElementById("streetwear").style.borderBottom = "0px solid #000000";
+            document.getElementById("onSneaker").style.display = "block"
+            document.getElementById("onStreetwear").style.display = "none";
+            document.getElementById("onWatches").style.display = "none";
+            document.getElementById("onHandbags").style.display = "none";
+            document.getElementById("onJewellery").style.display = "none";
           }
-       
+          
+        document.getElementById("sneakerBox").style.display = "flex";
+        document.getElementById("streetwearBox").style.display = "none";
+        document.getElementById("watchBox").style.display = "none";
+        document.getElementById("handBox").style.display = "none";
+        document.getElementById("jewelleryBox").style.display = "none";
     }
 
     StreetwearActive = () => {
         this.setState({ isSneaker: false});
         this.setState({ isStreetwear: true});
+        this.setState({ isWatch: false});
+        this.setState({ isHandbag: false});
+        this.setState({ isJewellery: false});
         if(
-          document.getElementById("streetwear")
+          document.getElementById("streetwear") &&
+          document.getElementById("onStreetwear")
           ){
-            document.getElementById("streetwear").style.borderBottom = "10px solid #000000";
-            document.getElementById("sneakers").style.borderBottom = "0px solid #000000";
+            document.getElementById("onSneaker").style.display = "none"
+            document.getElementById("onStreetwear").style.display = "block";
+            document.getElementById("onWatches").style.display = "none";
+            document.getElementById("onHandbags").style.display = "none";
+            document.getElementById("onJewellery").style.display = "none";
           }
+        
+          document.getElementById("sneakerBox").style.display = "none";
+          document.getElementById("streetwearBox").style.display = "flex";
+          document.getElementById("watchBox").style.display = "none";
+          document.getElementById("handBox").style.display = "none";
+          document.getElementById("jewelleryBox").style.display = "none";
+    }
+
+    WatchesActive = () => {
+        this.setState({ isSneaker: false});
+        this.setState({ isStreetwear: false});
+        this.setState({ isWatch: true});
+        this.setState({ isHandbag: false});
+        this.setState({ isJewellery: false});
+        if(
+          document.getElementById("watches") &&
+          document.getElementById("onWatches")
+          ){
+            document.getElementById("onSneaker").style.display = "none"
+            document.getElementById("onStreetwear").style.display = "none";
+            document.getElementById("onWatches").style.display = "block";
+            document.getElementById("onHandbags").style.display = "none";
+            document.getElementById("onJewellery").style.display = "none";
+          }
+
+          document.getElementById("sneakerBox").style.display = "none";
+          document.getElementById("streetwearBox").style.display = "none";
+          document.getElementById("watchBox").style.display = "flex";
+          document.getElementById("handBox").style.display = "none";
+          document.getElementById("jewelleryBox").style.display = "none";
+    }
+
+    HandbagsActive = () => {
+        this.setState({ isSneaker: false});
+        this.setState({ isStreetwear: false});
+        this.setState({ isWatch: false});
+        this.setState({ isHandbag: true});
+        this.setState({ isJewellery: false});
+        if(
+          document.getElementById("streetwear") &&
+          document.getElementById("onStreetwear")
+          ){
+            document.getElementById("onSneaker").style.display = "none"
+            document.getElementById("onStreetwear").style.display = "none";
+            document.getElementById("onWatches").style.display = "none";
+            document.getElementById("onHandbags").style.display = "block";
+            document.getElementById("onJewellery").style.display = "none";
+          }
+
+          document.getElementById("sneakerBox").style.display = "none";
+          document.getElementById("streetwearBox").style.display = "none";
+          document.getElementById("watchBox").style.display = "none";
+          document.getElementById("handBox").style.display = "flex";
+          document.getElementById("jewelleryBox").style.display = "none";
+    }
+
+    JewelleryActive = () => {
+        this.setState({ isSneaker: false});
+        this.setState({ isStreetwear: false});
+        this.setState({ isWatch: false});
+        this.setState({ isHandbag: false});
+        this.setState({ isJewellery: true});
+        if(
+          document.getElementById("streetwear") &&
+          document.getElementById("onStreetwear")
+          ){
+            document.getElementById("onSneaker").style.display = "none"
+            document.getElementById("onStreetwear").style.display = "none";
+            document.getElementById("onWatches").style.display = "none";
+            document.getElementById("onHandbags").style.display = "none";
+            document.getElementById("onJewellery").style.display = "block";
+          }
+
+          document.getElementById("sneakerBox").style.display = "none";
+          document.getElementById("streetwearBox").style.display = "none";
+          document.getElementById("watchBox").style.display = "none";
+          document.getElementById("handBox").style.display = "none";
+          document.getElementById("jewelleryBox").style.display = "flex";
     }
 
     render() {
@@ -35,21 +135,45 @@ export class Featured extends Component {
                 <div id="navList">
                     <div id="sneakers" className="navItem" 
                     onClick={this.SneakerActive}>
-                        SNEAKERS
+                        <p className="navText">SNEAKERS</p>
+                        <div className="underline" style={{display: "block"}} id="onSneaker"></div>
                     </div>
                     <div id="streetwear" className="navItem"
                     onClick={this.StreetwearActive}>
-                        STREETWEAR
+                        <p className="navText">STREETWEAR</p>
+                        <div className="underline" id="onStreetwear"></div>
                     </div>
-                    <div className="navItem">
-                        WATCHES
+                    <div id="watches" className="navItem"
+                    onClick={this.WatchesActive}>
+                        <p className="navText">WATCHES</p>
+                        <div className="underline" id="onWatches"></div>
                     </div>
-                    <div className="navItem">
-                        HANDBAGS
+                    <div id="handbags" className="navItem"
+                    onClick={this.HandbagsActive}>
+                        <p className="navText">HANDBAGS</p>
+                        <div className="underline" id="onHandbags"></div>
                     </div>
-                    <div className="navItem">
-                        JEWELLERY
+                    <div id="jewellery" className="navItem"
+                    onClick={this.JewelleryActive}>
+                        <p className="navText">JEWELLERY</p>
+                        <div className="underline" id="onJewellery"></div>
                     </div>
+                </div>
+
+                <div id="sneakerBox" >
+                    Sneakers
+                </div>
+                <div id="streetwearBox">
+                    Streetwear
+                </div>
+                <div id="watchBox">
+                    Watches
+                </div>
+                <div id="handBox">
+                    Handbags
+                </div>
+                <div id="jewelleryBox">
+                    Jewellery
                 </div>
             </div>
         );
