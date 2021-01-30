e833e1c737c752067986f1c618078977ec21f59c
Victorien Garrigues <victoriengarrigues@VictoriensAir2.com>
2021-01-30 09:20:07 -0500

    structure and start of header

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+    render() {
+      return (
+          <div id="totalHeader"></div>
+      );
+    }
+}
+
+export default Header;
