2eff18c71f1585ce7fdc485ca45198291d7e33bf
Victorien Garrigues <victoriengarrigues@VictoriensAir2.com>
2021-01-30 15:03:21 -0500

    pulling

--- src/components/featured.js
+++ src/components/featured.js
@@ -3,7 +3,25 @@ import React, { Component } from "react";
 export class Featured extends Component {
     render() {
         return (
-            <div id="totalFeatured">this is  the list</div>
+            <div id="totalFeatured">
+                <div id="navList">
+                    <div className="navItem">
+                        <p>Sneakers</p>
+                    </div>
+                    <div className="navItem">
+                        <p>Streetwear</p>
+                    </div>
+                    <div className="navItem">
+                        <p>Watches</p>
+                    </div>
+                    <div className="navItem">
+                        <p>Handbags</p>
+                    </div>
+                    <div className="navItem">
+                        <p>Jewellery</p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
