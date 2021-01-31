46cf29c5a6b7dfc74834017c297429486df399cf
Victorien Garrigues <victoriengarrigues@VictoriensAir2.com>
2021-01-31 05:12:13 -0500

    f

--- src/components/featured.js
+++ src/components/featured.js
@@ -218,7 +218,7 @@ export class Featured extends Component {
                                     </div>
                                 </div>
                             </div>
-                            )}
+                            )} 
                         </div>
                     </div>
                 </div>
